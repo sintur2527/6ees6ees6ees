30b235b13b5b75342f92940354308e3e58ff34f9
Soumith Inturi <sintur2527@gmail.com>
2019-06-03 12:38:28 -0400

    Finish Implementing Bees with ES6 style

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,14 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee{
+  constructor(age, color, food, job, honeyPot) {
+    super(color, food);
+    this.age = 10;
+    this.job = 'make honey';
+    this.honeyPot = 0;
+  }
+  makeHoney() {
+    this.honeyPot++;
+  }
+  giveHoney() {
+    this.honeyPot--;
+  }
 };
--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,15 @@
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee{
+  constructor(age, color, food, job, canFly, treasureChest) {
+    super(food, treasureChest);
+    this.age = 40;
+    this.color = 'grey';
+    this.job = 'gamble';
+    this.canFly = false;
+  }
+  forage() {
+    return 'I am too old, let me play cards instead';
+  }
+  gamble(treasure) {
+    this.treasureChest.push(treasure);
+  }
 };
